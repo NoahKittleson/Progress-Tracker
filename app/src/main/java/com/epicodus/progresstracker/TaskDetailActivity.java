ac9f9e0d89f100c17d171a3babff26b850473f3d
Jeffrey Hackford and Noah Kittleson <jeffrey@>
2016-07-21 14:32:00 -0700

    The most beautiful crash Ive ever seen

--- app/src/main/java/com/epicodus/progresstracker/TaskDetailActivity.java
+++ app/src/main/java/com/epicodus/progresstracker/TaskDetailActivity.java
@@ -2,27 +2,42 @@ package com.epicodus.progresstracker;
 
 import android.graphics.Color;
 import android.graphics.PointF;
+import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import com.hookedonplay.decoviewlib.DecoView;
 import com.hookedonplay.decoviewlib.charts.SeriesItem;
 import com.hookedonplay.decoviewlib.charts.SeriesLabel;
 import com.hookedonplay.decoviewlib.events.DecoEvent;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class TaskDetailActivity extends AppCompatActivity {
+    @Bind(R.id.chartView) DecoView mArcView;
+    @Bind(R.id.textView2) TextView mTextView;
+    SeriesItem seriesItem4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_detail);
+        ButterKnife.bind(this);
 
         DecoView arcView = (DecoView) findViewById(R.id.chartView);
+        //DecoView arcView2 = (DecoView) findViewById(R.id.chartView2);
 
         SeriesItem seriesItem1 = new SeriesItem.Builder(Color.argb(127, 64, 196, 0))
                 .setRange(0, 100, 0)
+                .setShowPointWhenEmpty(false)
                 .setInset(new PointF(100f, 200f))
                 .setLineWidth(32f)
+                .setInitialVisibility(false)
+                .setSpinDuration(9000)
 //                .setSeriesLabel(new SeriesLabel.Builder("Percent %.0f%%")
 //                        .setColorBack(Color.argb(218, 0, 0, 0))
 //                        .setColorText(Color.argb(255, 255, 255, 255))
@@ -30,18 +45,27 @@ public class TaskDetailActivity extends AppCompatActivity {
                 .build();
         SeriesItem seriesItem2 = new SeriesItem.Builder(Color.argb(127, 196, 64, 0))
                 .setRange(0, 100, 0)
+                .setShowPointWhenEmpty(false)
                 .setInset(new PointF(0f, 300f))
                 .setLineWidth(32f)
+                .setSpinDuration(10000)
+                .setInitialVisibility(false)
                 .build();
         SeriesItem seriesItem3 = new SeriesItem.Builder(Color.argb(127, 32, 64, 90))
                 .setRange(0, 100, 0)
                 .setInset(new PointF(200f, 100f))
                 .setLineWidth(32f)
+                .setSpinDuration(11000)
+                .setInitialVisibility(false)
+                .setShowPointWhenEmpty(false)
                 .build();
-        SeriesItem seriesItem4 = new SeriesItem.Builder(Color.argb(127, 196, 64, 200))
+        seriesItem4 = new SeriesItem.Builder(Color.argb(127, 196, 64, 200))
                 .setRange(0, 100, 0)
                 .setInset(new PointF(300f, 0f))
                 .setLineWidth(32f)
+                .setSpinDuration(12000)
+                .setInitialVisibility(false)
+                .setShowPointWhenEmpty(false)
                 .build();
 
 
@@ -60,16 +84,53 @@ public class TaskDetailActivity extends AppCompatActivity {
                 .setDuration(2000)
                 .build());
 
-        arcView.addEvent(new DecoEvent.Builder(-100).setIndex(series1Index).setDelay(3000).build());
-        arcView.addEvent(new DecoEvent.Builder(100).setIndex(series2Index).setDelay(2000).build());
-        arcView.addEvent(new DecoEvent.Builder(-100).setIndex(series3Index).setDelay(2333).build());
-        arcView.addEvent(new DecoEvent.Builder(100).setIndex(series4Index).setDelay(2666).build());
-        arcView.addEvent(new DecoEvent.Builder(100).setIndex(series5Index).setDelay(3000).build());
-        arcView.addEvent(new DecoEvent.Builder(-100).setIndex(series6Index).setDelay(2000).build());
-        arcView.addEvent(new DecoEvent.Builder(100).setIndex(series7Index).setDelay(2333).build());
-        arcView.addEvent(new DecoEvent.Builder(-100).setIndex(series8Index).setDelay(2666).build());
+        arcView.addEvent(new DecoEvent.Builder(-100).setIndex(series1Index).setDelay(3000).setColor(Color.parseColor("#FFEAEAEA")).build());
+        arcView.addEvent(new DecoEvent.Builder(100).setIndex(series2Index).setDelay(2000).setColor(Color.parseColor("#FFEAEAEA")).build());
+        arcView.addEvent(new DecoEvent.Builder(-100).setIndex(series3Index).setDelay(2333).setColor(Color.parseColor("#FFEAEAEA")).build());
+        arcView.addEvent(new DecoEvent.Builder(100).setIndex(series4Index).setDelay(2666)
+                .setColor(Color.parseColor("#FFEAEAEA"))
+                .setListener(new DecoEvent.ExecuteEventListener() {
+                    @Override
+                    public void onEventStart(DecoEvent event) {
+                        Log.d("TaskDetailActivity", "Hide of DecoView Starting");
+                    }
 
-//        arcView.addEvent(new DecoEvent.Builder(10).setIndex(series1Index).setDelay(12000).build());
+                    @Override
+                    public void onEventEnd(DecoEvent event) {
+                        Log.d("TaskDetailActivity", 9/0 + "");
+                    }
+                }).build());
+//        arcView2.addEvent(new DecoEvent.Builder(100).setIndex(series5Index).setDelay(3000).build());
+//        arcView2.addEvent(new DecoEvent.Builder(-100).setIndex(series6Index).setDelay(2000).build());
+//        arcView.addEvent(new DecoEvent.Builder(100).setIndex(series7Index).setDelay(2333).build());
+//        arcView.addEvent(new DecoEvent.Builder(-100).setIndex(series8Index).setDelay(2666).build());
+
+        arcView.configureAngles(360, 315);
+        //arcView.configureAngles(360, 157);
+
+        arcView.setVisibility(View.VISIBLE);
+
+        final String format = "%.0f%%";
+
+        seriesItem4.addArcSeriesItemListener(new SeriesItem.SeriesItemListener() {
+            @Override
+            public void onSeriesItemAnimationProgress(float percentComplete, float currentPosition) {
+                if (percentComplete > .93) {
+                    int crash = 1/0;
+                }
+                if (format.contains("%%")) {
+                    float percentFilled = ((currentPosition - seriesItem4.getMinValue()) / (seriesItem4.getMaxValue() - seriesItem4.getMinValue()));
+                    mTextView.setText(String.format(format, percentFilled * 100f));
+                } else {
+                    mTextView.setText(String.format(format, currentPosition));
+                }
+            }
+
+            @Override
+            public void onSeriesItemDisplayProgress(float percentComplete) {
+
+            }
+        });
 
     }
 }
